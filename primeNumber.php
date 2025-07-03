4812a82af3cb9ff4254b24765049d695a754e136
mahidulkabir <mahidulkabir8@gmail.com>
2025-07-03 09:42:14 +0600

    all code updated

---
+++ primeNumber.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h2>Check Your Prime Number Here </h2>
+  <form method='post'>
+  enter a number<input type="text" name="fname">
+    <input type="submit" name="submit">
+</form>
+
+
+<?php
+if(isset($_POST["submit"]))
+{
+  $b=$_POST["fname"];
+  $c=0;
+if($b==0 || $b==1){
+  echo "Not Prime or Composite";
+  exit();
+}
+for($i=2;$i<$b;$i++)
+{
+  if($b%$i==0){
+     $c++;
+break;
+  }
+ 
+};
+if($c==0){
+
+  echo "$b prime number";
+}
+else{
+
+  echo "$b not a prime number";
+}
+}
+?>
+</body>
+</html>
+
